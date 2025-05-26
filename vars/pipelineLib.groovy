bd4cd028933a642da71771cc522fd99060e0084a
Mariam Hossam <76646258+mariamhossamdiab@users.noreply.github.com>
2025-05-26 21:29:33 +0300

    Update pipelineLib.groovy

---
+++ vars/pipelineLib.groovy
@@ -0,0 +1,26 @@
+ef call() {
+    pipeline {
+        agent any
+
+        environment {
+            XYZ = 'ITI ITI ITI'
+            IMAGE_NAME = 'mariamhossam00/jenkins-library'
+        }
+
+       stages {
+        stage('Build Java & Docker') {
+            steps {
+                echo 'Building Java app and Docker image...'
+                buildJavaApp(env.IMAGE_NAME)
+            }
+        }
+
+            stage('Push Docker Image') {
+                steps {
+                    echo 'Pushing Docker image to DockerHub'
+                    pushDockerImage(env.IMAGE_NAME)
+                }
+            }
+        }
+    }
+}
